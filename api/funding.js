52ac13e98779afc965d0c37b6db1e322a9814153
Kramer <kramer@bitcoinintel.xyz>
2026-03-08 04:23:52 +0000

    Add Open Interest + Funding Rate cards (CoinGlass Hobbyist tier)

---
+++ api/funding.js
@@ -0,0 +1,14 @@
+const CG_KEY = '2880d014de714442baf6258c0cb4f786';
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  try {
+    const r = await fetch('https://open-api.coinglass.com/public/v2/funding?symbol=BTC', {
+      headers: { coinglassSecret: CG_KEY }
+    });
+    const d = await r.json();
+    res.json(d);
+  } catch (e) {
+    res.status(500).json({ code: 'error', msg: e.message });
+  }
+}
